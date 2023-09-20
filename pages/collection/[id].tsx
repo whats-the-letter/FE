a06d2adcff87da0f4e9ae1393686df379476cc87
yeokyoung song <spolice0324@gmail.com>
2023-09-20 23:53:31 +0900

    feat : 페이지 라우팅 구현

---
+++ pages/collection/[id].tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      돌아가는 엘피에서 탭투 뷰 카드 클릭 시 이동한 페이지 화면 내 컬렉션에 모인
+      앨범들이 보이는 페이지
+    </div>
+  );
+}
---
+++ pages/getInfo.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+    return <div>
+        닉네임 받기
+
+        메인 배경색 선택
+        컬렉션 배경색 선택 (if 메인이랑 분리할 경우)
+        
+        엘피판 색 선택
+        엘피판 가운데 아이콘 선택
+        
+        </div>;
+  }
+  
