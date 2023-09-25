63117e1698c03a84a0d4ba64987991ceca5b3c2e
yeokyoung song <spolice0324@gmail.com>
2023-09-25 22:27:26 +0900

    feat : 로그인 페이지 작업 및 scss module 연동 테스트

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,22 @@
+import React from "react";
+import Button from "../features/common/components/Button/Button";
+
 export default function Page() {
-  return <div>login</div>;
+  return (
+    <>
+      <div>
+        <img src="/assets/logo/dear-new-year-1.svg" alt="logo" />
+        <p>나만의 앨범 컬렉션을 만들어보아요!</p>
+        <p>
+          연말 연시 감사인사를 전할 사람을 위해 앨범을 제작하고 추천곡을 담아
+          보내요.
+        </p>
+        <p>
+          이번 푸른 용의해에는 특별한 방식으로 마음을 전달해보는 건 어떨까요?
+        </p>
+
+        <Button>시작하기</Button>
+      </div>
+    </>
+  );
 }
