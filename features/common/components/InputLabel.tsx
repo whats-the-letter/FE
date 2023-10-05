7c2d568db74cd25e8793888de17ff7b018f3658f
yeokyoung song <spolice0324@gmail.com>
2023-10-06 00:20:39 +0900

    refactor : 인풋라벨 컴포넌트 공용으로 분리

---
+++ features/common/components/InputLabel.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+
+interface InputlabelProps {
+  label: string;
+  errorMessage?: string;
+  required?: boolean;
+}
+
+function Inputlabel({
+  children,
+  label,
+  errorMessage,
+  required,
+}: PropsWithChildren<InputlabelProps>) {
+  return (
+    <div className="flex flex-col w-full space-y-2">
+      <span className=" font-bold">{label}</span>
+      {required}
+      {children}
+      <p className="text-customRed text-sm pt-0.5">{errorMessage}</p>
+    </div>
+  );
+}
+
+export default Inputlabel;
