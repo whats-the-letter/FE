2cb05448f242ed207d411e7b045eb473adbbbc32
yeokyoung song <spolice0324@gmail.com>
2023-10-06 00:20:17 +0900

    feat : 인풋 컴포넌트 공용으로 분리

---
+++ features/common/components/Input.tsx
@@ -0,0 +1,42 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+import { classNames } from "../utils/classNames";
+
+interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "className"> {
+  errorMessage?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { errorMessage, ...props },
+  ref
+) {
+  const showError = errorMessage !== "";
+
+  return (
+    <>
+      <div
+        className={classNames(
+          "relative w-full h-fit border-none",
+          "focus-within:outline-none",
+          showError && "border-red-500 focus-within:border-red-500"
+        )}
+      >
+        <input
+          {...props}
+          ref={ref}
+          className={classNames(
+            "w-full h-10 input-underline placeholder:text-left",
+            "focus:outline-none",
+            "placeholder:text-gray-300"
+          )}
+          {...props}
+        />
+      </div>
+      {errorMessage !== "" && (
+        <p className="text-red-500 text-sm">{errorMessage}</p>
+      )}
+    </>
+  );
+});
+
+export default Input;
