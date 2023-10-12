a783b88fab099167ace50a22aceb3a40ab23c3cd
yeokyoung song <spolice0324@gmail.com>
2023-10-12 17:52:32 +0900

    feat : editor UI 구현 및 레이아웃 확인

--- pages/newalbum.tsx
+++ pages/newalbum.tsx
@@ -1,15 +1,116 @@
-export default function Page() {
-    return <div>
-        
-        새 앨범 작성하는 페이지
+import { InputHTMLAttributes, forwardRef } from "react";
+import { classNames } from "../features/common/utils/classNames";
+import { useForm } from "react-hook-form";
 
-        - 키워드를 선택해주세요
-        - 배경을 선택해주세요
-        - 문구를 선택해주세요
-        - 음악을 선택해주세요 
-        - 편지를 작성해주세요
-        - 앨범 완성 확인
-        - 앨범 공유하기 모달
-         </div>;
+export default function Page() {
+  const {
+    formState: { errors },
+    watch,
+    register,
+    handleSubmit,
+  } = useForm<{
+    editor: string;
+  }>({
+    mode: "onChange",
+    defaultValues: {
+      editor: "editor-love",
+    },
+  });
+
+  const isEditor = watch("editor");
+
+  return (
+    <>
+      <div className="flex flex-col w-full h-screen items-center justify-center z-10 m-auto space-y-10 max-w-screen-sm">
+        <form>
+          <div className="flex flex-col justify-center items-center m-auto px-8 space-y-10 my-20">
+            <div className="flex w-full space-y-6 justify-end ">
+              <button className="flex items-center h-10 w-fit ">
+                <span className="font-pretendard mx-2">다음 </span>
+                <img src="/assets/icons/chevron_right.svg" alt="arrow-right" />
+              </button>
+            </div>
+            <span className="text-lg text-center font-pretendard">
+              커버를 선택해주세요
+            </span>
+            <img src="/assets/editor/editor-love.svg" alt="cover" />
+
+            <div className="flex flex-row gap-8">
+              <EditorRadioButton
+                {...register("editor", {})}
+                id="editor-love"
+                name="editor"
+                value="editor-love"
+                label="애정"
+                selected={isEditor}
+              ></EditorRadioButton>
+              <EditorRadioButton
+                {...register("editor", {})}
+                id="editor-money"
+                name="editor"
+                value="editor-money"
+                label="재물"
+                selected={isEditor}
+              ></EditorRadioButton>
+              <EditorRadioButton
+                {...register("editor", {})}
+                id="editor-success"
+                name="editor"
+                value="editor-success"
+                label="성공"
+                selected={isEditor}
+              ></EditorRadioButton>
+              <EditorRadioButton
+                {...register("editor", {})}
+                id="editor-health"
+                name="editor"
+                value="editor-health"
+                label="건강"
+                selected={isEditor}
+              ></EditorRadioButton>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+interface EditorRadioButtonProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: React.ReactNode;
+  id: string;
+  selected: boolean | string;
+
+  name: string;
+  value: string;
+  defaultChecked?: boolean;
+}
+
+// eslint-disable-next-line react/display-name
+const EditorRadioButton = forwardRef<HTMLInputElement, EditorRadioButtonProps>(
+  ({ label, selected, id, name, value, defaultChecked, ...props }, ref) => {
+    return (
+      <>
+        <input
+          id={id}
+          value={value}
+          name={name}
+          type="radio"
+          {...props}
+          ref={ref}
+          required
+          className="hidden"
+        ></input>
+        <label
+          htmlFor={id}
+          className={classNames(
+            "font-pretendard text-[#9e9e9e]",
+            selected === value && "text-black"
+          )}
+        >
+          {label}
+        </label>
+      </>
+    );
   }
-  
+);
