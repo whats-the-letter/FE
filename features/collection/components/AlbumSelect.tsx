7308d0e270857f6cb40b0e2fac2cc61a2fa5d6fa
yeokyoung song <spolice0324@gmail.com>
2023-10-18 18:46:43 +0900

    refactor: 앨범 셀렉트 컴포넌트 디자인 사항 반영, 다음 버튼 임시제거

--- features/collection/components/AlbumSelect.tsx
+++ features/collection/components/AlbumSelect.tsx
@@ -15,43 +15,34 @@ const AlbumSelect: React.FC<AlbumSelectProps> = ({
   onAlbumChange,
 }) => {
   return (
-    <div className="flex flex-col justify-center items-center m-auto px-8 space-y-10 my-20">
-      <div className="flex w-full space-y-6 justify-end">
-        <button className="flex items-center h-10 w-fit" type="submit">
-          <span className="font-pretendard mx-2">다음 </span>
+    <>
+      <div className="flex flex-col justify-center items-center m-auto px-4 space-y-10 my-20">
+        <span className="text-lg text-center font-pretendard">
+          커버를 선택해주세요
+        </span>
+
+        <div>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/assets/icons/chevron_right.svg" alt="arrow-right" />
-        </button>
-      </div>
-      <span className="text-lg text-center font-pretendard">
-        커버를 선택해주세요
-      </span>
+          <img src={albumSelection[isEditor]} alt="editor" />
+        </div>
 
-      <div>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src={albumSelection[isEditor]}
-          alt="editor"
-          className="w-60 h-60"
-        />
+        <div className="flex flex-row gap-8 whitespace-nowrap">
+          {Object.keys(albumSelection).map((editor) => (
+            <EditorRadioButton
+              name="editor"
+              key={editor}
+              id={editor}
+              label={labelMap[editor]}
+              selected={isEditor}
+              onChange={() => onAlbumChange(editor)}
+            />
+          ))}
+        </div>
+        <p className="text-[12px] text-gray-400 text-center font-pretendard font-thin">
+          중앙에는 플레이리스트의 커버 이미지가 들어갑니다.
+        </p>
       </div>
-
-      <div className="flex flex-row gap-8">
-        {Object.keys(albumSelection).map((editor) => (
-          <EditorRadioButton
-            name="editor"
-            key={editor}
-            id={editor}
-            label={labelMap[editor]}
-            selected={isEditor}
-            onChange={() => onAlbumChange(editor)}
-          />
-        ))}
-      </div>
-      <p className="text-[12px] text-gray-400 text-center font-pretendard font-thin">
-        중앙에는 플레이리스트의 커버 이미지가 들어갑니다.
-      </p>
-    </div>
+    </>
   );
 };
 
