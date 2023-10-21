444f24ba60834df105e15393ba61ee14a108567d
yeokyoung song <spolice0324@gmail.com>
2023-10-21 18:49:58 +0900

    refactor : RoundRadioButton 오류 수정
    - 컴포넌트 분리 시 컬러 불러오기 실패 오류 수정
    - map이 아닌 단일 선언 문 4개씩으로 그냥 통일함
    - 이유 : 이후 폼에 해당하는 에셋이 컬러가 아닌 luck, 2024.. 등등의 형태이기 때문에 통일이 불가능이라고 판단
    - 컬러를 제대로 못불러오는 오류까지 발생으로 수정 진행함

---
+++ features/collection/components/RoundRadioButton.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
+import { classNames } from "../../common/utils/classNames";
+
+interface RoundRadioButtonProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+  isChecked: boolean;
+  className: string;
+  name: string;
+  value: string;
+}
+// eslint-disable-next-line react/display-name
+const RoundRadioButton = forwardRef<HTMLInputElement, RoundRadioButtonProps>(
+  ({ label, isChecked, id, className, name, value, ...props }, ref) => {
+    return (
+      <>
+        <label
+          htmlFor={id}
+          className={classNames(
+            "relative rounded-full h-10 w-10 border-1 cursor-pointer",
+            className,
+            isChecked && "border border-black"
+          )}
+        >
+          <div className="flex items-center">
+            <input
+              id={id}
+              value={value}
+              name={name}
+              type="radio"
+              {...props}
+              ref={ref}
+              required
+              className="hidden"
+            ></input>
+            {isChecked && (
+              <img
+                src="/assets/icons/selected.svg"
+                alt="selected"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10 h-10"
+              />
+            )}
+          </div>
+        </label>
+      </>
+    );
+  }
+);
+
+export default RoundRadioButton;
---
+++ pages/info.tsx
@@ -0,0 +1,172 @@
+import { useForm } from "react-hook-form";
+import Inputlabel from "../features/common/components/InputLabel";
+import Input from "../features/common/components/Input";
+import RoundRadioButton from "../features/collection/components/RoundRadioButton";
+
+export default function Page() {
+  const {
+    formState: { errors },
+    watch,
+    register,
+    handleSubmit,
+  } = useForm<{
+    nickname: string;
+    lpBackground: string;
+    lpDesign: string;
+  }>({
+    mode: "onChange",
+    defaultValues: {
+      lpBackground: "pink",
+      lpDesign: "lp-luck",
+    },
+  });
+
+  const isBackground = watch("lpBackground");
+  const isLpDesign = watch("lpDesign");
+
+  const onSubmit = (data: {
+    nickname: string;
+    lpBackground: string;
+    lpDesign: string;
+  }) => {
+    console.log(data);
+  };
+
+  const onError = () => {};
+
+  return (
+    <>
+      <div className="flex flex-col w-full h-screen items-center justify-center z-10 m-auto space-y-10 max-w-screen-sm">
+        <div className="w-full h-full flex flex-col justify-center m-auto">
+          <form onSubmit={handleSubmit(onSubmit, onError)}>
+            <div className=" z-10 m-auto px-8 space-y-10 my-20">
+              <div className="flex flex-col w-full space-y-6 ">
+                {/* 닉네임 입력 */}
+                <Inputlabel
+                  label="닉네임"
+                  required
+                  errorMessage={errors.nickname?.message}
+                >
+                  <Input
+                    {...register("nickname", {
+                      required: "닉네임은 필수 입력입니다.",
+                      minLength: {
+                        value: 2,
+                        message: "닉네임은 2글자 이상이어야 합니다.",
+                      },
+                      maxLength: {
+                        value: 6,
+                        message: "닉네임은 6글자 이하여야 합니다.",
+                      },
+                    })}
+                    type="text"
+                    placeholder="닉네임을 입력해주세요."
+                  ></Input>
+                </Inputlabel>
+
+                {/* 배경색 선택 */}
+                <Inputlabel
+                  label="배경색 선택"
+                  required
+                  errorMessage={errors.lpBackground?.message}
+                >
+                  <div className="flex flex-row space-x-4">
+                    <RoundRadioButton
+                      {...register("lpBackground")}
+                      label="bg-pink"
+                      isChecked={isBackground === "pink"}
+                      id="bg-pink"
+                      value="pink"
+                      className="bg-custom_pink"
+                      name="lpBackground"
+                    />
+                    <RoundRadioButton
+                      {...register("lpBackground")}
+                      label="bg-blue"
+                      isChecked={isBackground === "blue"}
+                      id="bg-blue"
+                      name="lpBackground"
+                      value="blue"
+                      className="bg-custom_skyblue"
+                    />
+                    <RoundRadioButton
+                      {...register("lpBackground")}
+                      label="bg-silver"
+                      isChecked={isBackground === "silver"}
+                      id="bg-silver"
+                      name="lpBackground"
+                      value="silver"
+                      className="bg-custom_silver"
+                    />
+                    <RoundRadioButton
+                      {...register("lpBackground")}
+                      label="bg-gold"
+                      isChecked={isBackground === "gold"}
+                      id="bg-gold"
+                      name="lpBackground"
+                      value="gold"
+                      className="bg-custom_gold"
+                    />
+                  </div>
+                </Inputlabel>
+
+                {/* LP 디자인 선택 */}
+                <Inputlabel
+                  label="LP 디자인 선택"
+                  required
+                  errorMessage={errors.lpDesign?.message}
+                >
+                  <div className="flex flex-row space-x-4 pb-10">
+                    <RoundRadioButton
+                      {...register("lpDesign")}
+                      label="lp-luck"
+                      isChecked={isLpDesign === "lp-luck"}
+                      id="lp-luck"
+                      name="lpDesign"
+                      value="lp-luck"
+                      className="bg-custom_red"
+                    />
+                    <RoundRadioButton
+                      {...register("lpDesign")}
+                      label="lp-dragon"
+                      isChecked={isLpDesign === "lp-dragon"}
+                      id="lp-dragon"
+                      name="lpDesign"
+                      value="lp-dragon"
+                      className="bg-custom_blue"
+                    />
+                    <RoundRadioButton
+                      {...register("lpDesign")}
+                      label="lp-dny"
+                      isChecked={isLpDesign === "lp-dny"}
+                      id="lp-dny"
+                      name="lpDesign"
+                      value="lp-dny"
+                      className="bg-custom_black"
+                    />
+                    <RoundRadioButton
+                      {...register("lpDesign")}
+                      label="lp-2024"
+                      isChecked={isLpDesign === "lp-2024"}
+                      id="lp-2024"
+                      name="lpDesign"
+                      value="lp-2024"
+                      className="bg-custom_gold"
+                    />
+                  </div>
+                </Inputlabel>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full h-10 bg-black text-white rounded"
+              >
+                미리보기
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
