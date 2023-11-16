af4d157077ae8d9ab9f4301a0fcb7147b84151e8
yeokyoung song <spolice0324@gmail.com>
2023-11-16 21:57:02 +0900

    feat : 썸네일 예시 이미지 추가, data 파일로 이동

---
+++ features/utils/data.ts
@@ -0,0 +1,94 @@
+export const albumSelection = {
+  "editor-love": "/assets/editor/editor-love.svg",
+  "editor-money": "/assets/editor/editor-money.svg",
+  "editor-success": "/assets/editor/editor-success.svg",
+  "editor-health": "/assets/editor/editor-health.svg",
+};
+
+export const labelMap = {
+  "editor-love": "사랑",
+  "editor-money": "재물",
+  "editor-success": "성공",
+  "editor-health": "건강",
+};
+
+export const phrasesSelection = {
+  "editor-1": "/assets/editor/editor-1.svg",
+  "editor-2": "/assets/editor/editor-2.svg",
+  "editor-3": "/assets/editor/editor-3.svg",
+  "editor-4": "/assets/editor/editor-4.svg",
+  "editor-5": "/assets/editor/editor-5.svg",
+};
+
+export const letterSelection = {
+  "editor-love-letter": "/assets/letter/letter-love.svg",
+  "editor-money-letter": "/assets/letter/letter-money.svg",
+  "editor-success-letter": "/assets/letter/letter-success.svg",
+  "editor-health-letter": "/assets/letter/letter-health.svg",
+};
+
+export const playListSelection = [
+  {
+    id: 0,
+    name: "크리스마스 캐롤 캐빈",
+    artist: "여경이",
+    youtubeUrlId: "YAsAQgoh52I",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 1,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 2,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 3,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 4,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 5,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 6,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 7,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+  {
+    id: 8,
+    name: "크리스마스 캐롤2 에센셜",
+    artist: "현지",
+    youtubeUrlId: "a_80o2lDYec",
+    thumbnail: "/assets/thumbnail_example.svg",
+  },
+];
