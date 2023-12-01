b3481449d12867798322eab07c2f6b5724ecd675
yeokyoung song <spolice0324@gmail.com>
2023-12-01 19:49:30 +0900

    feat : 로그인 필요 서비스 유도 페이지 구현

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function LoginPage() {
   return (
     <>
-      <div className="flex flex-col w-full h-screen items-center justify-center z-10 m-auto p-4 px-10 space-y-10 ">
+      <div className="flex flex-col w-full h-screen items-center justify-center z-10 m-auto p-4 px-10 space-y-10 font-pretendard">
         <img src="/assets/main/main-logo.svg" alt="logo" />
-        <div className="flex flex-col items-center justify-center z-10 py-4 font-pretendard font-semibold text-sm text-center space-y-0.5">
-          <span>연말 연시 감사인사를 전할 사람을 위해</span>
-          <span> 앨범을 제작하고 추천곡을 담아 보내보세요.</span>
-          <span>이번 푸른 용의 해에는 특별한 방식으로</span>
-          <span>마음을 전달해보는 건 어떨까요?</span>
+        <h1 className="text-xl font-semibold"> 로그인이 필요한 서비스입니다.</h1>
+        <div className="flex flex-col items-center justify-center z-10 py-3 font-normal text-sm text-center space-y-0.5">
+          <span>Dear New Year을 찾아주셔서 감사합니다.</span>
+          <span>서비스 이용을 위해서는 로그인이 필요합니다.</span>
+          <span>로그인 후 다양한 서비스를 이용해보세요!</span>
         </div>
 
         <button className="bg-[#FAE100] rounded-md text-center w-full max-w-sm h-10 p-2 px-4 flex items-center">
