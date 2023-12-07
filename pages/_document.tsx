814b8b9b7eb263a1ac3537b76177115bc568f798
yeokyoung song <spolice0324@gmail.com>
2023-12-07 17:26:07 +0900

    feat : 카카오 sdk 등록

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
