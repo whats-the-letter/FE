70255d60339d0846a5a38c1a74fba96074e6fd97
yeokyoung song <spolice0324@gmail.com>
2023-12-07 17:25:46 +0900

    feat : Redux 보일러 플레이트 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,27 @@ import React from "react";
 import "../public/styles/globals.css";
 import type { AppProps } from "next/app";
 import { OverlayProvider } from "@toss/use-overlay";
+import { Provider } from "react-redux";
+import store from "../store/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <OverlayProvider>
-      <Component {...pageProps} />
-    </OverlayProvider>
+    <>
+      <Head>
+        <title>Dear New Year</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta charSet="utf-8"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <OverlayProvider>
+          <Component {...pageProps} />
+        </OverlayProvider>
+      </Provider>
+    </>
   );
 }
---
+++ store/store.ts
@@ -0,0 +1,18 @@
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
