dbaecf2f0114f30e39c67ff12765ee7578abb2f7
yeokyoung song <spolice0324@gmail.com>
2023-12-28 17:05:27 +0900

    fix : next-auth session, token 설정
    - 이것저것 계속 시도...

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,103 +1,147 @@
 import axios from "axios";
 import { NextAuthOptions, User, Account, Profile } from "next-auth";
-import { JWT } from "next-auth/jwt";
+
 import NextAuth from "next-auth/next";
+import { Provider } from "next-auth/providers/index";
 
 import KakaoProvider from "next-auth/providers/kakao";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID!,
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
+
   callbacks: {
-    async jwt({ token, user, account }: any) {
-      if (account && user) {
-        token.accessToken = account.accessToken;
-        token.accessTokenExpires = account.expires_at;
-        token.refreshToken = account.refreshToken;
-        token.user = user;
-
-        const nowTime = Math.round(Date.now() / 1000);
-        const shouldRefreshTime =
-          (token.accessTokenExpires as number) - 10 * 60 - nowTime;
-        if (shouldRefreshTime < 0) {
-          return token;
-        }
-        return refreshAccessToken(token);
-      }
-
+    async jwt({ token, account, user }: any) {
       if (account) {
-        return {
-          accessToken: account.accessToken,
-          accessTokenExpires: account.expires_at,
-          refreshToken: account.refreshToken,
-          user,
-        };
+        token.accessToken = account.access_token;
+        if (account.provider === "kakao") {
+          token.provider = "kakao";
+        }
       }
-
       return token;
     },
-
-    async session({ session, token, user }) {
-      session.user = token.user as User;
+    async session({ session, token }: any) {
       session.accessToken = token.accessToken;
-      session.refreshToken = token.refreshToken;
-      session.accessTokenExpires = token.accessTokenExpires;
-      session.error = token.error;
+      session.provider = token.provider;
+
       return session;
     },
   },
-
-  pages: {
-    signIn: "/login",
-  },
-
-  redirect: async (url: any, baseUrl: any) =>
-    url.startsWith(baseUrl) ? Promise.resolve(url) : Promise.resolve(baseUrl),
 };
 
-async function refreshAccessToken(token: JWT) {
-  try {
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/renew`;
+export default (req: any, res: any) => NextAuth(req, res, authOptions);
 
-    const params = {
-      grant_type: "refresh_token",
-      refresh_token: token.refreshToken,
-    };
+// const oAuthProviders: Provider[] = [
+//   KakaoProvider({
+//     clientId:process.env.KAKAO_CLIENT_ID!,
+//     clientSecret: process.env.KAKAO_CLIENT_SECRET!,
 
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
+//     profile(profile) {
+//       return {
+//         id: profile.id.toString(),
+//         email: profile.kakao_account.email,
+//         accessToken: profile.access_Token,
+//       };
+//     },
+//   }),
+// ];
 
-    const res = await axios.post(url, params.toString(), {
-      headers,
-      auth: {
-        username: process.env.KAKAO_CLIENT_ID!,
-        password: process.env.KAKAO_CLIENT_SECRET!,
-      },
-    });
+// const nextAuthOptions: NextAuthOptions = {
+//   providers: [...oAuthProviders],
 
-    const refreshedToken = await res.data;
+//   callbacks: {
+//     async session({ session, token }) {
+//       session.user.id = token.id;
+//       session.accessToken = token.accessToken;
+//       console.log(session);
+//       console.log("세션-액세스토큰", session.accessToken);
+//       return session;
+//     },
+//     async jwt({ token, user, account }) {
+//       if (user) {
+//         token.id = user.id;
+//         token.accessToken = account?.access_token;
+//         console.log("토큰-액세스토큰", token.accessToken);
+//       }
+//       return token;
+//     },
+//   },
+// };
+//   callbacks: {
+//     async jwt({ token, account, user }) {
+//       if (account) {
+//         return {
+//           accessToken: account.access_Token,
+//           accessTokenExpires: account.expires_at,
+//           refreshToken: account.refreshToken,
+//           user,
+//         };
+//       }
 
-    if (res.status !== 200) {
-      throw refreshedToken;
-    }
-    return {
-      ...token,
-      accessToken: refreshedToken.access_token,
-      accessTokenExpires:
-        Math.round(Date.now() / 1000) + refreshedToken.expires_in,
-      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
-    };
-  } catch (error) {
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
+//       const nowTime = Math.round(Date.now() / 1000);
+//       const shouldRefreshTime =
+//         (token.accessTokenExpires as number) - 10 * 60 - nowTime;
+//       // 토큰이 만료되지 않았을때는 원래사용하던 토큰을 반환
+//       if (shouldRefreshTime > 0) {
+//         return token;
+//       }
+//       return refreshAccessToken(token);
+//     },
 
-export default NextAuth(authOptions);
+//     async session({ session, token }) {
+//       session.user = token.user as User;
+//       session.accessToken = token.accessToken;
+//       session.accessTokenExpires = token.accessTokenExpires;
+//       session.error = token.error;
+//       return session;
+//     },
+//   },
+// };
+
+// // AccessToken이 만료되면 refreshToken을 사용해서 다시 받아오는 함수
+// async function refreshAccessToken(token: JWT) {
+//   try {
+//     const url = "https://kauth.kakao.com/oauth/token";
+
+//     const params = {
+//       grant_type: "refresh_token",
+//       refresh_token: token.refreshToken,
+//     };
+
+//     const headers = {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     };
+
+//     const res = await axios.post(url, null, {
+//       headers,
+//       params,
+//       auth: {
+//         username: process.env.CLIENT_ID as string,
+//         password: process.env.CLIENT_SECRET as string,
+//       },
+//     });
+
+//     const refreshedTokens = await res.data;
+
+//     if (res.status !== 200) {
+//       throw refreshedTokens;
+//     }
+
+//     return {
+//       ...token,
+//       accessToken: refreshedTokens.access_token,
+//       accessTokenExpires:
+//         Math.round(Date.now() / 1000) + refreshedTokens.expires_in,
+//       refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+//     };
+//   } catch (err) {
+//     return {
+//       ...token,
+//       error: "RefreshAccessTokenError",
+//     };
+//   }
+// }
