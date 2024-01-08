493e753dbe321ccf8f1b52aa75d8b271f2fc51b9
yeokyoung song <spolice0324@gmail.com>
2024-01-08 14:02:35 +0900

    feat : 세션, auth interface 추가

--- pages/api/auth/auth.d.ts
+++ pages/api/auth/auth.d.ts
@@ -1,11 +1,10 @@
 import NextAuth, { DefaultSession, User } from "next-auth";
 
-
 declare module "next-auth" {
   interface Session extends DefaultSession {
-    user?: {
-      id?: string;
-    } & DefaultSession["user"];
     accessToken: string;
+    provider: string;
+    sub?: string;
+    email?: string;
   }
 }
