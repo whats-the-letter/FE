e494502fb0c72e02075d9fe7838e79e5a545e0ac
yeokyoung song <spolice0324@gmail.com>
2024-02-05 15:04:39 +0900

    feat : 메인 임시 라우팅 페이지 추가

--- pages/main/[id].tsx
+++ pages/main/[id].tsx
@@ -1,20 +1,9 @@
-import Loading from "@/components/units/Loading";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
-
-import { useRouter } from "next/router";
 export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
-
   return (
     <div>
       <h1>Main Page for User {}</h1>
 
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-
-      <div>
-      </div>
+      <div></div>
     </div>
   );
 }
