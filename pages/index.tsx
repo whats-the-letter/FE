d8dd1e64fc9dcad71be0c994a4e58a585da63209
yeokyoung song <spolice0324@gmail.com>
2024-03-12 14:01:13 +0900

    feat: 랜딩 카피 및 ui 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col w-full h-screen items-center justify-center z-10 m-auto p-4 px-10 space-y-10 ">
-        <img src="/assets/main/main-logo.svg" alt="logo" />
-        <div className="flex flex-col items-center justify-center z-10 py-4 font-pretendard font-semibold text-sm text-center space-y-0.5">
-          <span>연말 연시 감사인사를 전할 사람을 위해</span>
-          <span> 앨범을 제작하고 추천곡을 담아 보내보세요.</span>
-          <span>이번 푸른 용의 해에는 특별한 방식으로</span>
-          <span>마음을 전달해보는 건 어떨까요?</span>
+      <div className="flex flex-col flex-1 w-full h-screen items-center justify-center z-10 m-auto p-4 px-10 gap-10 ">
+        <div>
+          <img src="/assets/main/main-logo.svg" alt="logo" />
+          <span className="flex flex-col items-center justify-center z-10 font-pretendard font-semibold text-sm text-center space-y-0.5 mt-12">
+            What&apos;s The Letter?과 함께 <br />
+            앨범을 제작하고 추천곡을 담아 보내요!
+            <br />
+            소중한 사람에게 오늘은 특별한 방식으로
+            <br />
+            마음을 전달해보는 건 어떨까요?
+            <br />
+          </span>
+          <p className="text-custom_gray text-[10px] mt-4">
+            * 이벤트 한정시즌에는 새로운 앨범이 기다리고 있을지도 몰라요!
+          </p>
         </div>
-
         <button
-          className="bg-[#FAE100] 
+          className="bg-[#FAE100]  
           rounded-md text-center w-64 max-w-sm h-10 p-2 px-4 flex items-center"
         >
           <img src="/assets/icons/kakao.svg" alt="kakao" />
