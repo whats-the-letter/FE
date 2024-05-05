e5b7f9fc166f71b4cbcedd51cd85c19a9a0f57de
yeokyoung song <spolice0324@gmail.com>
2024-05-05 23:54:55 +0900

    feat: 음악 목록 조회 api 연결

--- features/components/units/Select/MusicList.tsx
+++ features/components/units/Select/MusicList.tsx
@@ -1,92 +1,146 @@
 import React, { forwardRef, useEffect, useState } from "react";
-import axios from "axios";
-import SearchBar from "../SearchBar";
+import axios, { AxiosResponse } from "axios";
+
 import Image from "next/image";
 import emptyMusic from "@/assets/icons/empty_music.svg";
-import selectedMusic from "@/assets/icons/selected_music.svg";
+import selected from "@/assets/icons/selected_music.svg";
 import unselectedMusic from "@/assets/icons/unselected_music.svg";
 import playButton from "@/assets/icons/play_button.svg";
 import stopButton from "@/assets/icons/stop.svg";
+import SearchBar from "@/components/units/SearchBar";
 
-interface MusicListProps {
-  playListSelection: {
-    id: number;
-    name: string;
-    artist: string;
-    youtubeUrlId: string;
-    thumbnail: string;
-    category: string;
-  }[];
-  onMusicChange: (musicData: {
-    id: number;
-    artist: string;
-    name: string;
-    thumbnail: string;
-  }) => void;
+export interface MusicListProps {
+  musicList: MusicProps[];
+  onMusicChange: (music: MusicProps[
+    
+  ]) => void;
+}
+
+export interface MusicProps {
+  musicArtist: string;
+  musicId: string;
+  musicName: string;
+  tags: string[];
+  youtubeUrlId: string;
 }
 
 //eslint-disable-next-line react/display-name
 const MusicList = forwardRef<HTMLInputElement, MusicListProps>(
-  ({ playListSelection, onMusicChange }, ref) => {
-    const [videoData, setVideoData] = useState(null);
-    const [selectedVideoId, setSelectedVideoId] = useState(
-      playListSelection[0].youtubeUrlId
-    );
+  ({ onMusicSelect }, ref) => {
+    const [musicData, setMusicData] = useState<MusicProps[]>([]);
+    const [selectedMusicId, setSelectedMusicId] = useState<string | null>(null);
     const [isPlaying, setIsPlaying] = useState(false);
-    const [filteredPlayList, setFilteredPlayList] = useState(playListSelection);
+    const [filteredPlayList, setFilteredPlayList] = useState<MusicProps[]>([]);
+
+    useEffect(() => {
+      const getMusicData = async () => {
+        try {
+          const response: AxiosResponse<MusicListProps> =
+            await axios.get("/api/music/list");
+
+          if (response.status !== 200) {
+            throw new Error("음악 목록 가져오기 실패");
+          }
+
+          setMusicData(response.data.musicList);
+        } catch (error) {
+          return [];
+        }
+      };
+      getMusicData();
+    }, []);
+
+    useEffect(() => {
+      setFilteredPlayList(musicData);
+    }, [musicData]);
 
     const handleSearch = (keyword: string) => {
-      const filteredList = playListSelection.filter(
+      const filteredList = musicData.filter(
         (item) =>
-          item.name.toLowerCase().includes(keyword.toLowerCase()) ||
-          item.artist.toLowerCase().includes(keyword.toLowerCase())
+          item.musicName.toLowerCase().includes(keyword.toLowerCase()) ||
+          item.musicArtist.toLowerCase().includes(keyword.toLowerCase())
       );
       setFilteredPlayList(filteredList);
     };
 
-    const fetchVideoData = async (videoId: string) => {
+    //   const playMusic = async (musicId: string) => {
+    //     try {
+    //       console.log(musicId);
+    //       const response = await axios.get(
+    //         `https://www.googleapis.com/youtube/v3/videos?id=${musicId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&part=snippet`
+    //       );
+    //       const selectedMusicData = response.data.items[0];
+    //       setMusicData(selectedMusicData);
+    //     } catch (error) {
+    //       console.error("음악 재생 실패", error);
+    //     }
+    //   };
+
+    const playMusic = async (youtubeUrlId: string) => {
       try {
-        const response = await axios.get(
-          `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&part=snippet`
-        );
-        setVideoData(response.data.items[0]);
+        const videoId = youtubeUrlId;
+        setIsPlaying(false);
+        setSelectedMusicId(youtubeUrlId);
+
+        const iframe = document.getElementById(
+          "youtubePlayer"
+        ) as HTMLIFrameElement;
+        if (iframe) {
+          iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&start=0.5`;
+          iframe.classList.remove("hidden");
+        }
       } catch (error) {
-        console.error("Error fetching video data:", error);
+        console.error("음악 재생 실패", error);
       }
     };
 
     useEffect(() => {
-      if (selectedVideoId) {
-        fetchVideoData(selectedVideoId);
-      } else if (playListSelection.length > 0) {
-        setSelectedVideoId(playListSelection[0].youtubeUrlId);
+      if (selectedMusicId) {
+        playMusic(selectedMusicId);
+      } else if (musicData.length > 0) {
+        setSelectedMusicId(musicData[0].youtubeUrlId);
       }
-    }, [selectedVideoId, playListSelection]);
+    }, [selectedMusicId, musicData]);
 
-    const handleMusicBoxClick = (videoId: string) => {
-      const selectedMusic = playListSelection.find(
-        (item) => item.youtubeUrlId === videoId
+    const handleMusicBoxClick = (musicId: string) => {
+      const selectedMusic = musicData.find(
+        (item) => item.youtubeUrlId === musicId
       );
-      setSelectedVideoId(videoId);
-      // onMusicChange({
-      // music: videoId,
-      // artist: selectedMusic.artist,
-      // name: selectedMusic.name,
-      // thumbnail: selectedMusic.thumbnail,
-      // });
+      setSelectedMusicId(musicId);
       setIsPlaying(false);
+      if (selectedMusic) {
+        onMusicSelect([selectedMusic]);
+      }
     };
 
     const handlePlayButtonClick = () => {
       setIsPlaying(true);
     };
 
-    if (!selectedVideoId || !videoData) {
-      return null;
+    if (!selectedMusicId || !musicData) {
+      return <div>로딩 중...</div>;
     }
-    const selectedVideo = playListSelection.find(
-      (item) => item.youtubeUrlId === selectedVideoId
+
+    const selectedVideos = musicData.find(
+      (item) => item.youtubeUrlId === selectedMusicId
     );
+
+    //   useEffect(() => {
+    //     if (selectedMusicId) {
+    //       playMusic(selectedMusicId);
+    //     } else if (musicData.length > 0) {
+    //       setSelectedMusicId(musicData[0].youtubeUrlId);
+    //     }
+    //   }, [selectedMusicId, musicData]);
+
+    // const handleMusicBoxClick = (musicId: string) => {
+    //   const selectedMusic = musicData.find(
+    //     (item) => item.youtubeUrlId === musicId
+    //   );
+    //   setSelectedMusicId(musicId);
+    //   //노래 변경
+    //   setIsPlaying(false);
+    // };
 
     return (
       <>
@@ -111,16 +165,12 @@ const MusicList = forwardRef<HTMLInputElement, MusicListProps>(
           ) : (
             filteredPlayList.map((item) => (
               <div
-                key={item.id}
+                key={item.musicId}
                 className="flex gap-4 items-center w-full h-[70px] hover:bg-gray-200 cursor-pointer px-2"
                 onClick={() => handleMusicBoxClick(item.youtubeUrlId)}
               >
-                {selectedVideoId === item.youtubeUrlId ? (
-                  <Image
-                    src={selectedMusic}
-                    alt="selected-music"
-                    className="w-4"
-                  />
+                {selectedMusicId === item.youtubeUrlId ? (
+                  <Image src={selected} alt="selected-music" className="w-4" />
                 ) : (
                   <Image
                     src={unselectedMusic}
@@ -129,27 +179,31 @@ const MusicList = forwardRef<HTMLInputElement, MusicListProps>(
                   />
                 )}
                 <div className="flex flex-grow items-center justify-between">
-                  <img
+                  {/* <img
                     src={item.thumbnail}
                     alt="thumbnail"
                     className="w-12 h-12"
-                  />
+                  /> */}
                   <div className="flex-grow flex flex-col items-start justify-start font-pretendard mx-8">
-                    <h2 className="text-sm">{item.name}</h2>
-                    <h2 className="text-custom_gray text-sm">{item.artist}</h2>
+                    <h2 className="text-sm">{item.musicName}</h2>
+                    <h2 className="text-custom_gray text-sm">
+                      {item.musicArtist}
+                    </h2>
                   </div>
                 </div>
               </div>
             ))
           )}
         </div>
-        {selectedVideo && (
+
+        {selectedVideos && (
           <div className="bg-black w-full h-16 flex flex-row justify-between items-center px-2">
             {/* <img
               src={selectedVideo.thumbnail}
               alt="thumbnail"
-              className="w-9 mr-4"
-            /> */}
+        className="w-9 mr-4"
+            />
+             */}
             <div className="flex flex-col justify-start items-start w-40 overflow-hidden">
               <div
                 className={`w-fit marquee ${isPlaying ? "playing" : ""}`}
@@ -159,11 +213,11 @@ const MusicList = forwardRef<HTMLInputElement, MusicListProps>(
                 }}
               >
                 <span className="text-white font-pretendard text-sm">
-                  {selectedVideo.name}
+                  {selectedVideos.musicName}
                 </span>
               </div>
               <span className="text-white font-pretendard text-center text-xs">
-                {selectedVideo.artist}
+                {selectedVideos.musicArtist}
               </span>
             </div>
 
@@ -215,7 +269,7 @@ const MusicList = forwardRef<HTMLInputElement, MusicListProps>(
             className="hidden"
             width="0"
             height="0"
-            src={`https://www.youtube.com/embed/${selectedVideoId}?autoplay=1&start=0.5`}
+            src={`https://www.youtube.com/embed/${selectedMusicId}?autoplay=1&start=0.5`}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           />
