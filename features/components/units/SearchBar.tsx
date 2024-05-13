88f474a61e17b3e7292cc0cbe354345a5e62b760
yeokyoung song <spolice0324@gmail.com>
2024-05-13 23:43:35 +0900

    feat: 음악 서치바, 결과 없음 이미지 수정

--- features/components/units/SearchBar.tsx
+++ features/components/units/SearchBar.tsx
@@ -13,21 +13,21 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
 
   return (
     <div
-      className="w-full h-8 bg-transparent rounded-full flex items-center justify-between border border-black px-3
+      className="w-full h-8 bg-transparent flex items-center justify-between
     "
     >
-      <input
-        type="text"
-        className="w-full h-full bg-transparent text-left text-sm font-pretendard font-normal placeholder:custom_gray placeholder:font-pretendard placeholder:font-normal focus:outline-none
-        "
-        placeholder="가수/제목 검색"
-        onChange={handleInputChange}
-      />
       <Image
         src={search}
         alt="search"
-        className="w-4 h-4 
+        className="w-4 h-4 mr-2
       "
+      />
+      <input
+        type="text"
+        className="w-full h-full bg-transparent border-b border-black text-left text-sm font-pretendard font-normal placeholder:custom_gray placeholder:font-pretendard placeholder:font-normal focus:outline-none
+        "
+        placeholder=""
+        onChange={handleInputChange}
       />
     </div>
   );
