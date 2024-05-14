1e7c510f5c4d442622c52dd22e5c8f143bb9de19
yeokyoung song <spolice0324@gmail.com>
2024-05-14 16:36:53 +0900

    feat: 전역 유저 관리 훅 구현

---
+++ features/store/useUserInfoStore.tsx
@@ -0,0 +1,18 @@
+import { UserInfo } from "@/types";
+import { create } from "zustand";
+
+export type UserInfoStoreProps = {
+  userInfo: UserInfo;
+  setUserInfo: (userInfo: UserInfo) => void;
+};
+
+const useUserInfoStore = create<UserInfoStoreProps>((set) => ({
+  userInfo: {
+    userId: "",
+    userName: "",
+    email: "",
+  },
+  setUserInfo: (userInfo) => set({ userInfo }),
+}));
+
+export default useUserInfoStore;
---
+++ features/types/index.ts
@@ -0,0 +1,5 @@
+export type UserInfo = {
+  userId: string;
+  userName: string;
+  email: string;
+};
